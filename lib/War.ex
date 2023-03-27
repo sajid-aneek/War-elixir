54c44d27f1bbf197be0c6cd83ce834db1bcf4b2d
Sajid <mdsajidrahman.khan@ryerson.ca>
2023-03-27 12:08:28 -0400

    uopdate

--- lib/War.ex
+++ lib/War.ex
@@ -1,45 +1,62 @@
 defmodule War do
-  @moduledoc """
-    Documentation for `War`.
-  """
-
   @doc """
-    Function that deals the cards to both players and simulates the game of War.
-    Returns the name of the winning player.
+  The main module to the challenge.
+  This module exposes a deal/1 function to play the game.
   """
+
+  @ace_rank 14
+
   def deal(shuf) do
-    {p1, p2} = Enum.split(shuf, 26)
-    play(p1, p2, "Player 1")
+    {p1, p2} = shuf |> Enum.chunk_every(2) |> Enum.reverse()
+
+    {Enum.reverse(p1), Enum.reverse(p2)}
+    |> case do
+      {p1, p2} -> game_loop(p1, p2, [])
+    end
   end
 
-  def play(p1, p2, winner) do
-    case {p1, p2} do
-      {[], _} -> "Player 2"
-      {_, []} -> "Player 1"
-      {[p1_card | p1_tail], [p2_card | p2_tail]} ->
-        if p1_card > p2_card do
-          play(p1_tail ++ [p1_card, p2_card], p2_tail, winner)
-        else if p1_card < p2_card do
-          play(p1_tail, p2_tail ++ [p2_card, p1_card], winner)
-        else
-          play(war(p1_tail, p2_tail, [p1_card], [p2_card]), [], winner)
+  defp game_loop(p1, p2, tied) do
+    case {p1, p2, tied} do
+      {[], [], _} ->
+        tied |> Enum.sort_by(&elem(&1, 1), :desc) |> Enum.map(&value_revert/1)
+
+      {[], _, _} ->
+        p2 ++ tied |> Enum.sort_by(&elem(&1, 1), :desc) |> Enum.map(&value_revert/1)
+
+      {_, [], _} ->
+        p1 ++ tied |> Enum.sort_by(&elem(&1, 1), :desc) |> Enum.map(&value_revert/1)
+
+      {[x | xs], [y | ys], _} ->
+        cards = [x, y] ++ tied |> Enum.sort_by(&elem(&1, 1), :desc)
+
+        case compare_cards(x, y) do
+          :gt ->
+            game_loop(xs ++ cards, ys, [])
+
+          :lt ->
+            game_loop(xs, ys ++ cards, [])
+
+          :eq ->
+            if xs != [] and ys != [] do
+              [fc_down1 | xs] = xs
+              [fc_down2 | ys] = ys
+              game_loop(xs, ys, cards ++ [fc_down1, fc_down2])
+            else
+              game_loop(xs, ys, cards)
+            end
         end
-      end
     end
   end
 
-  def war(p1, p2, war_p1, war_p2) do
-    [c1_1, c1_2 | p1_tail] = p1
-    [c2_1, c2_2 | p2_tail] = p2
-    case {c1_2, c2_2} do
-      {nil, nil} -> war(war_p1, war_p2, [c1_1], [c2_1])
-      {_, nil} -> p1_tail ++ war_p1 ++ war_p2 ++ [c1_1, c2_1, c1_2]
-      {nil, _} -> p2_tail ++ war_p1 ++ war_p2 ++ [c1_1, c2_1, c2_2]
-      {c1_2, c2_2} when c1_2 > c2_2 ->
-        play(p1_tail ++ war_p1 ++ war_p2 ++ [c1_1, c2_1, c1_2, c2_2], p2_tail, "Player 1")
-      {c1_2, c2_2} when c1_2 < c2_2 ->
-        play(p1_tail ++ war_p1 ++ war_p2 ++ [c1_1, c2_1, c2_2, c1_2], p2_tail, "Player 2")
-      {c1_2, c2_2} -> war(p1_tail, p2_tail, war_p1 ++ [c1_1, c1_2], war_p2 ++ [c2_1, c2_2])
+  defp compare_cards({x, x_rank}, {y, y_rank}) do
+    cond do
+      x_rank > y_rank -> :gt
+      x_rank < y_rank -> :lt
+      true -> :eq
     end
+  end
+
+  defp value_revert({card, _}) do
+    card
   end
 end
